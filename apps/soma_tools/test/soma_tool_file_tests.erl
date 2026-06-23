7a9588799c39b7bf87b3f7be6a4ed1d0ee118009
Joe Li <li.joe@outlook.com>
2026-06-23 17:03:18 +0800

    test: soma_tool_file_write then file_read roundtrips

--- apps/soma_tools/test/soma_tool_file_tests.erl
+++ apps/soma_tools/test/soma_tool_file_tests.erl
@@ -13,6 +13,17 @@ test_file_read_returns_bytes() ->
 file_read_returns_bytes_test() ->
     test_file_read_returns_bytes().
 
+test_file_write_then_read_roundtrips() ->
+    Root = make_temp_root(),
+    Bytes = <<"bytes written then read back">>,
+    Path = <<"roundtrip.txt">>,
+    Ctx = #{root => Root},
+    {ok, _} = soma_tool_file_write:invoke(#{path => Path, bytes => Bytes}, Ctx),
+    ?assertEqual({ok, Bytes}, soma_tool_file_read:invoke(#{path => Path}, Ctx)).
+
+file_write_then_read_roundtrips_test() ->
+    test_file_write_then_read_roundtrips().
+
 make_temp_root() ->
     Base = filename:basedir(user_cache, "soma_tool_file_tests"),
     Unique = integer_to_list(erlang:unique_integer([positive])),
