a54365c339a7609c84d5115ca0c3a425f56c1e5a
Joe Li <li.joe@outlook.com>
2026-07-01 22:44:01 +0800

    feat: auto-resume durable runs on boot

--- apps/soma_runtime/src/soma_app.erl
+++ apps/soma_runtime/src/soma_app.erl
@@ -7,7 +7,27 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    soma_sup:start_link().
+    case soma_sup:start_link() of
+        {ok, _SupPid} = Started ->
+            maybe_resume_interrupted(),
+            Started;
+        Error ->
+            Error
+    end.
 
 stop(_State) ->
     ok.
+
+maybe_resume_interrupted() ->
+    case application:get_env(soma_runtime, event_store_log, undefined) of
+        undefined ->
+            ok;
+        _Path ->
+            soma_run_auto_resume:resume_interrupted(event_store_pid())
+    end.
+
+event_store_pid() ->
+    Children = supervisor:which_children(soma_sup),
+    {soma_event_store, Pid, _Type, _Mods} =
+        lists:keyfind(soma_event_store, 1, Children),
+    Pid.
